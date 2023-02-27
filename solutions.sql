2d7841a6b8b76fd87761d62a60f7ab2816721612
Kirstin Gosling <kirst_gosling@yahoo.com>
2023-02-27 22:26:08 +0100

    complete lab

--- solutions.sql
+++ solutions.sql
@@ -56,21 +56,163 @@ ORDER BY amount ASC;
 
 # 8. What are the unique values of k_symbol in the order table?
 # Note: There shouldn't be a table name order, since order is reserved from the ORDER BY clause. You have to use backticks to escape the order table name.
+SELECT DISTINCT(k_symbol)
+FROM bank.order;
+
+# 9. In the order table, what are the order_ids of the client with the account_id 34?
+SELECT order_id
+FROM bank.order
+WHERE account_id = 34;
+
+# 10. In the order table, which account_ids were responsible for orders between order_id 29540 and order_id 29560 (inclusive)?
+SELECT DISTINCT(account_id)
+FROM bank.order
+WHERE  
+	order_id BETWEEN 29540 AND 29560;
+
+# 11. In the order table, what are the individual amounts that were sent to (account_to) id 30067122?
 SELECT *
-FROM order;
+FROM bank.order;
+
+SELECT amount
+FROM bank.order
+WHERE account_to = 30067122;
+
+# 12. In the trans table, show the trans_id, date, type and amount of the 10 first transactions from account_id 793 in chronological order, from newest to oldest.
+SELECT trans_id, date, type, amount
+FROM trans
+WHERE account_id = 793
+ORDER BY date DESC
+LIMIT 10;
+
+# 13. In the client table, of all districts with a district_id lower than 10, how many clients are from each district_id? Show the results sorted by the district_id in ascending order.
+SELECT * 
+FROM client;
+
+SELECT district_id, COUNT(client_id)
+FROM client
+WHERE district_id < 10
+GROUP BY district_id
+ORDER BY district_id ASC;
+
+# 14. In the card table, how many cards exist for each type? Rank the result starting with the most frequent type.
+SELECT *
+FROM card;
+
+SELECT distinct(type), COUNT(type)
+FROM card
+GROUP BY type
+ORDER BY COUNT(type) DESC;
+
+# 15. Using the loan table, print the top 10 account_ids based on the sum of all of their loan amounts
+SELECT *
+FROM loan;
+
+SELECT account_id, SUM(amount) # add the amounts for the the account_ids
+FROM loan
+GROUP BY account_id 
+ORDER BY SUM(amount) DESC
+LIMIT 10;
+
+# 16. In the loan table, retrieve the number of loans issued for each day, before (excl) 930907, ordered by date in descending order.
+SELECT *
+FROM loan;
+
+SELECT date, COUNT(loan_id) AS loans
+FROM loan
+WHERE date < 930907
+GROUP BY date
+ORDER BY date DESC;
 
 
-SELECT account_id, type, ROUND(amount)
-CASE
-WHEN type = "VYDAJ" THEN "Otgoing"
-ELSE "Incoming"
-END AS  transaction_type
+# 17. In the loan table, for each day in December 1997, count the number of loans issued for each unique loan duration, ordered by date and duration, both in ascending order. You can ignore days without any loans in your output.
+SELECT *
+FROM loan;
+
+SELECT date, duration, COUNT(loan_id) AS loans
+FROM loan
+WHERE date = 9712;
+
+
+# 18. In the trans table, for account_id 396, sum the amount of transactions for each type (VYDAJ = Outgoing, PRIJEM = Incoming). Your output should have the account_id, the type and the sum of amount, named as total_amount. Sort alphabetically by type
+SELECT *
+FROM trans;
+
+SELECT account_id, type, SUM(amount) AS total_amount
+FROM trans
+WHERE account_id = 396
+GROUP BY type
+ORDER BY type ASC;
+
+#19. From the previous output, translate the values for type to English, rename the column to transaction_type, round total_amount down to an integer
+SELECT 
+	account_id, 
+	CASE
+		WHEN type = "VYDAJ" THEN "OUTGOING"
+        WHEN type = "PRIJEM" THEN "INCOMING"
+		ELSE "UNKNOWN"
+	END AS transaction_type,
+	FLOOR(SUM(amount)) AS total_amount
 FROM trans
 WHERE account_id = 396
 GROUP BY account_id, type
 ORDER BY account_id, type ASC;
 
+# 20. From the previous result, modify your query so that it returns only one row, with a column for incoming amount, outgoing amount and the difference
+SELECT *
+FROM trans;
 
+-- Basic solution
+SELECT
+	account_id,
+    FLOOR(SUM(IF(type = "PRIJEM", amount, 0))) AS incoming_amount,
+    FLOOR(SUM(IF(type = "VYDAJ", amount, 0))) AS outgoing_amount,
+    FLOOR(SUM(IF(type = "PRIJEM", amount, 0))) - FLOOR(SUM(IF(type = "VYDAJ", amount, 0))) AS diff
+FROM trans
+WHERE account_id = 396
+GROUP BY account_id;
+
+-- Using a subquery 
+SELECT 
+	account_id,
+    incoming_amount,
+    outgoing_amount,
+    incoming_amount - outgoing_amount AS diff
+FROM (
+	SELECT
+		account_id,
+		FLOOR(SUM(IF(type = "PRIJEM", amount, 0))) AS incoming_amount,
+		FLOOR(SUM(IF(type = "VYDAJ", amount, 0))) AS outgoing_amount
+	FROM trans
+	WHERE account_id = 396
+	GROUP BY account_id
+) AS amounts;
+
+# 21.
+
+-- Without subquery
+SELECT
+	account_id,
+    FLOOR(SUM(IF(type = "PRIJEM", amount, 0))) - FLOOR(SUM(IF(type = "VYDAJ", amount, 0))) AS diff
+FROM trans
+GROUP BY account_id
+ORDER BY diff DESC
+LIMIT 10;
+
+-- With subquery 
+SELECT 
+	account_id,
+    incoming_amount - outgoing_amount AS diff
+FROM (
+	SELECT
+		account_id,
+		FLOOR(SUM(IF(type = "PRIJEM", amount, 0))) AS incoming_amount,
+		FLOOR(SUM(IF(type = "VYDAJ", amount, 0))) AS outgoing_amount
+	FROM trans
+	GROUP BY account_id
+) AS amounts
+ORDER BY diff DESC
+LIMIT 10;
 
 ## Disadvantages of non-normalized 
 # 1. Data redundancy: Non-normalized databases often have duplicate data stored in multiple places, which can lead to inconsistencies and errors.
